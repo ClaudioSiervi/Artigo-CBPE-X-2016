b4ad04db566ac5a33fa84af13c05c3fe7d0d82f8
ClaudioSiervi <claudio.siervi@gmail.com>
2016-07-11 16:51:10 -0300

    gráficos com eixos fixos (tirei os eixos dinâmicos)

--- le_vazoes.m
+++ le_vazoes.m
@@ -39,7 +39,7 @@ function plotar(q)
     subplot(2,3,1);
     tam = size(q.pch1,1);
     plot(1:tam, q.pch1); 
-    qmax = ceil(max(q.pch1))+ 10; axis([0 tam 0 qmax]);
+    axis([0 tam 20 160]);
     xlabel('Meses');
     ylabel({'Vazão','(m³/s)'});
     legend('Vazão da Usina A', 'Location', 'northeast');
@@ -48,7 +48,7 @@ function plotar(q)
     subplot(2,3,2);
     tam = size(q.pch2,1);
     plot(1:tam, q.pch2);
-    qmax = ceil(max(q.pch2))+ 10; axis([0 tam 0 qmax]);
+    axis([0 tam 20 120]);
     xlabel('Meses');
     ylabel({'Vazão','(m³/s)'});
     legend('Vazão da Usina B', 'Location', 'northeast');
@@ -57,7 +57,7 @@ function plotar(q)
     subplot(2,3,3);
     tam = size(q.pch3,1);
     plot(1:tam, q.pch3);
-    qmax = ceil(max(q.pch3))+ 10; axis([0 tam 0 qmax]);
+    axis([0 tam 0 800]);
     xlabel('Meses');
     ylabel({'Vazão','(m³/s)'});
     legend('Vazão da Usina C', 'Location', 'northeast');
@@ -66,7 +66,7 @@ function plotar(q)
     subplot(2,3,4);
     tam = size(q.pch4,1);
     plot(1:tam, q.pch4);
-    qmax = ceil(max(q.pch4))+ 10; axis([0 tam 0 qmax]);
+    axis([0 tam 0 600]);
     xlabel('Meses');
     ylabel({'Vazão','(m³/s)'});
     legend('Vazão da Usina D', 'Location', 'northeast');
@@ -75,7 +75,7 @@ function plotar(q)
     subplot(2,3,5);
     tam = size(q.pch5,1);
     plot(1:tam, q.pch5);
-    qmax = ceil(max(q.pch5))+ 10; axis([0 tam 0 qmax]);
+    axis([0 tam 80 400]);
     xlabel('Meses');
     ylabel({'Vazão','(m³/s)'});
     legend('Vazão da Usina E', 'Location', 'northeast');
@@ -84,7 +84,7 @@ function plotar(q)
     subplot(2,3,6);
     tam = size(q.pch6,1);
     plot(1:tam, q.pch6);
-    qmax = ceil(max(q.pch6))+ 10; axis([0 tam 0 qmax]);
+    axis([0 tam 0 40]);
     xlabel('Meses');
     ylabel({'Vazão','(m³/s)'});
     legend('Vazão da Usina F', 'Location', 'northeast');

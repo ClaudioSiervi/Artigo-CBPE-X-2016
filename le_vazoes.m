d96dce73bfb572c0319a5b654a4734ffc91573da
ClaudioSiervi <claudio.siervi@gmail.com>
2016-07-11 17:19:42 -0300

    conserto das séries de datas criação de uma função (dentro de "le_vazoes") para ler as datas

--- le_vazoes.m
+++ le_vazoes.m
@@ -9,8 +9,7 @@ intervalo = 'C7:H1000';
 dados = xlsread(rota, aba, intervalo);
 
 q = struct('pch1',[],'pch2',[],'pch3',[], ...
-    'pch4',[], 'pch5',[], 'pch6',[]);
-
+    'pch4',[], 'pch5',[], 'pch6',[]); % vazões
 for i = 1:6
     
     fim = find(isnan(dados(:,i))==1, 1, 'first');   % encontra o 1º NaN
@@ -25,6 +24,36 @@ for i = 1:6
     q.(nome_campo) = vazao;
 end
 
-plota_vazoes(q)
+d=le_datas;
+%plota_vazoes(q)
+
+end
+
+
+function [d] = le_datas()
+
+arquivo = 'Dados_Artigo.xlsx';
+aba = 'vazões';
+caminho = pwd;
+rota = strcat(caminho,'\',arquivo);
+intervalo = 'J7:O1000';
+dados = readtable('Dados_Artigo.xlsx');
+%dados = xlsread(rota, aba, intervalo);
+
+d = struct('pch1',[],'pch2',[],'pch3',[], ...
+    'pch4',[], 'pch5',[], 'pch6',[]); % datas
+for i = 8:13
+    
+    fim = find(isnan(dados(:,i))==1, 1, 'first');   % encontra o 1º NaN
+    fim = fim - 1;                                  % último dado válido
+    if isempty(fim)                                 % se não tem NaN define o próprio vetor
+        fim = size(dados(:,i),1) ;
+    end
+    
+    nome_campo = strcat('pch', int2str(i));
+    
+    data = dados(1:fim, i);
+    d.(nome_campo) = data;
+end
 
 end

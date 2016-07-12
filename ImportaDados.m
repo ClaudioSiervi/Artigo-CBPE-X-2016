7ec044266f2d1e54545f68d5c25de76dad4de16b
ClaudioSiervi <claudio.siervi@gmail.com>
2016-07-12 16:43:54 -0300

    importando horas e premissas

--- ImportaDados.m
+++ ImportaDados.m
@@ -18,31 +18,36 @@ classdef  ImportaDados
     end
     
     methods
-        % Desvio padrão das potências anuais
+        
+        % Lê do excel as séries de vazão, horas e premissas 
         function obj = ImportaDados()
-            %dados = obj;
+            % SAÍDAS
+            %   obj -> objeto contendo todas as séries lidas do excel
+        
             rota = strcat(obj.caminho,'\',ImportaDados.arquivo);
             obj.vazoes = xlsread(rota, ImportaDados.aba1, ImportaDados.intervalo1');  
-           % obj.horas = xlsread(rota, ImportaDados.aba2, ImportaDados.intervalo2');  
-           % obj.premissas = xlsread(rota, ImportaDados.aba3, ImportaDados.intervalo3');  
-        
-           % AjustaSerie(dados, obj);
+            obj.horas = xlsread(rota, ImportaDados.aba2, ImportaDados.intervalo2');  
+            obj.premissas = xlsread(rota, ImportaDados.aba3, ImportaDados.intervalo3');   
         end
-    
-    
+        
+        % Retida as linhas vazias das séries lidas do excel
         function dados = LimpaSerie(obj, serie)
+            % ENTRADAS
+            %   serie -> serie de dados lidos do excel com linhas NaN
+            
+            % SAÍDAS
+            %   dados -> serie de dados sem as linhas NaN 
+        
+            fim = find(isnan(serie(:))==1, 1, 'first');         % encontra o 1º NaN
+            fim = fim - 1;                                      % último dado válido
+            if isempty(fim)                                     % se não tem NaN define o próprio vetor
+                fim = size(serie(:),1) ;
+            end
+            
+            dados = serie(1:fim);
 
-            %for i = 1:6
-                fim = find(isnan(serie(:))==1, 1, 'first');         % encontra o 1º NaN
-                fim = fim - 1;                                      % último dado válido
-                if isempty(fim)                                     % se não tem NaN define o próprio vetor
-                    fim = size(serie(:),1) ;
-                end
-                dados = serie(1:fim);
-                
-                varlist = {'obj', 'serie', 'fim'};
-                clear(varlist{:})
-
+            varlist = {'obj', 'serie', 'fim'};                  
+            clear(varlist{:})
         end
     end
 end
